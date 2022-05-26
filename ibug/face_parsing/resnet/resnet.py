c7c02c8b8e6fd1b5ea20860263c54e340655dfea
Yiming Lin <yiminglin@fb.com>
2022-05-26 12:10:45 +0100

    fix breaking changes in torchvision

--- ibug/face_parsing/resnet/resnet.py
+++ ibug/face_parsing/resnet/resnet.py
@@ -5,8 +5,6 @@ Changed padding to MixPad
 import torch
 from torch import Tensor
 import torch.nn as nn
-from torchvision.models.utils import load_state_dict_from_url
-from torchvision.models.resnet import model_urls
 from typing import Type, Any, Callable, Union, List, Optional
 
 
@@ -319,10 +317,6 @@ def _resnet(
     **kwargs: Any
 ) -> ResNet:
     model = ResNet(block, layers, **kwargs)
-    if pretrained:
-        state_dict = load_state_dict_from_url(model_urls[arch],
-                                              progress=progress)
-        model.load_state_dict(state_dict, strict=False)
     return model
 
 
